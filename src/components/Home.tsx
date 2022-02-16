d4ce62c2d2353cd86cc00e21680ba034ad09d85b
Menasi Ephrem <menasiephrem@gmail.com>
2022-02-16 13:11:55 +0300

    💄  added tailwind

---
+++ src/components/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="w-100 h-screen bg-red-50 flex justify-center items-center">
+      <span>Hello World</span>
+    </div>
+  );
+};
+
+export default Home;
